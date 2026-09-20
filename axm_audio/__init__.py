af302597a2c7da933c3a0ed8baa1fe54dde23c31
mike-axiom-mir <miketobi90@gmail.com>
2026-09-20 07:05:27 +0200

    Expose layered SFX public API

--- axm_audio/__init__.py
+++ axm_audio/__init__.py
@@ -12,17 +12,29 @@ from .core import (
     validate_atom,
     validate_cue,
 )
+from .layered import (
+    LAYERED_CUE_SCHEMA,
+    LAYERED_RECEIPT_SCHEMA,
+    render_layered_cue,
+    render_layered_wav,
+    validate_layered_cue,
+)
 
 __all__ = [
     "AUDIO_ATOM_SCHEMA",
     "AUDIO_CUE_SCHEMA",
     "RECEIPT_SCHEMA",
+    "LAYERED_CUE_SCHEMA",
+    "LAYERED_RECEIPT_SCHEMA",
     "AudioRecipeError",
     "build_receipt",
     "canonical_sha256",
     "render_atom",
     "render_cue",
     "render_wav",
+    "render_layered_cue",
+    "render_layered_wav",
     "validate_atom",
     "validate_cue",
+    "validate_layered_cue",
 ]
